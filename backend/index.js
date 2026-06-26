22b91399fcc1c4539ac70d600122fcd770de9c87
Hicham-Hal <154899492+Hicham-Hal@users.noreply.github.com>
2026-06-26 12:01:25 +0100

    Update route handling for production environment

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use('/api/analytics', analyticsRoute)
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, "/client/dist")))
 
-  app.get('*', (req, res)=>{
+  app.get('/{*splat}', (req, res)=>{
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   })
 }
